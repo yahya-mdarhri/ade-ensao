ab876719354ac282d287dc877ba49f1dd454dc0f
yahya-mdarhri <yahyamdarhri00@gmail.com>
2021-04-30 15:15:58 +0000

    v8

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
- <footer id="fh5co-footer" role="contentinfo" style="background-image: url(images/img_bg_4.jpg);">
+<footer id="fh5co-footer" role="contentinfo" style="background-image: url(images/img_bg_4.jpg);">
                 <div class="overlay"></div>
                 <div class="container">
                     <div class="row row-pb-md">
@@ -9,39 +9,14 @@ L'ADE est un organisme qui regroupe chaque année 1000 étudiants, car chaque é
 </p>
                         </div>
                         <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
-                            <a href="./index-filières.php"><h3>filières</h3></a>
-                            <ul class="fh5co-footer-links">
-                                
-                                <li><a href="./club%20civil.php">Génie Civil</a></li>
-                                <li><a href="./club%20informatique.php">Génie Informatique</a></li>
-                                <li><a href="./club%20indus.php">Génie Industriel</a></li>
-                                <li><a href="./club%20idscc.php">IDSCC</a></li>
-                                <li><a href="./club%20itirc.php">ITIRC</a></li>
-                                <li><a href="./sics.php">SICS</a></li>
+                            <a href="./leaders.php"><h3>LEADERS</h3></a>
                             </ul>
                         </div>
-        
-                     
-        
                         <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
-                            <a href="./index-clubs.php"><h3>clubs</h3></a>
-                            <ul class="fh5co-footer-links">
-                                <li><a href="./club%20altruisme.php">altruisme</a></li>
-                                <li><a href="./club%20culturel.php">culturel</a></li>
-                                <li><a href="./club%20enactus.php">enactus</a></li>
-                                <li><a href="./club%20essie.php">essie</a></li>
-                                <li><a href="./club%20futur%20ingenieurs.php">futur-ingenieurs</a></li>
-                                <li><a href="./club%20musical.php">musical</a></li>
-                                <li><a href="./club%20solidarit%C3%A9.php">solidarité</a></li>
-                                <li><a href="./club%20sportif.php">sportif</a></li>
-                                <li><a href="./club%20veolife.php">veolife</a></li>
-                                
-                                
-                               
+                            <a href="./magensao.php"><h3>MagEnsao</h3></a>
                             </ul>
                         </div>
-                     
-        
+                        
                         <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
                             <a href="./index-comités.php"><h3>comités</h3></a>
                             <ul class="fh5co-footer-links">
@@ -62,7 +37,39 @@ L'ADE est un organisme qui regroupe chaque année 1000 étudiants, car chaque é
                             </ul>
                         </div>
                     
-
+                        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+                            <a href="./index-clubs.php"><h3>clubs</h3></a>
+                            <ul class="fh5co-footer-links">
+                                <li><a href="./club%20altruisme.php">altruisme</a></li>
+                                <li><a href="./club%20culturel.php">culturel</a></li>
+                                <li><a href="./club%20enactus.php">enactus</a></li>
+                                <li><a href="./club%20essie.php">essie</a></li>
+                                <li><a href="./club%20futur%20ingenieurs.php">futur-ingenieurs</a></li>
+                                <li><a href="./club%20musical.php">musical</a></li>
+                                <li><a href="./club%20solidarit%C3%A9.php">solidarité</a></li>
+                                <li><a href="./club%20sportif.php">sportif</a></li>
+                                <li><a href="./club%20veolife.php">veolife</a></li>
+                                
+                                
+                               
+                            </ul>
+                        </div>
+                     
+                        
+                        
+                        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+                            <a href="./index-filières.php"><h3>filières</h3></a>
+                            <ul class="fh5co-footer-links">
+                                
+                                <li><a href="./club%20civil.php">Génie Civil</a></li>
+                                <li><a href="./club%20informatique.php">Génie Informatique</a></li>
+                                <li><a href="./club%20indus.php">Génie Industriel</a></li>
+                                <li><a href="./club%20idscc.php">IDSCC</a></li>
+                                <li><a href="./club%20itirc.php">ITIRC</a></li>
+                                <li><a href="./sics.php">SICS</a></li>
+                            </ul>
+                        </div>
+        
                         <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
                             <a href="./index-partenaires.php"><h3>partenaires</h3></a>
                             <ul class="fh5co-footer-links">
@@ -70,10 +77,12 @@ L'ADE est un organisme qui regroupe chaque année 1000 étudiants, car chaque é
                                 <li><a href="./adhoc.php">AD.HOC</a></li>
                             </ul>
                         </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
-                            <a href="./magensao.php"><h3>MagEnsao</h3></a>
-                            </ul>
-                        </div>
+        
+                      
+                        
+                     
+        
+                   
                 </div>
                 <div>
         
