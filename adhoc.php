65fec607b63bbc95bda1e17dc4388d4be1a185a6
yahya-mdarhri <yahyamdarhri00@gmail.com>
2021-05-02 22:28:04 +0000

    Update adhoc.php

--- adhoc.php
+++ adhoc.php
@@ -24,7 +24,7 @@
                 <div class="container">
                     <div class="row animate-box">
                         <div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
-                          <a href="https://alensao.com/ad-hoc"><h1>AD.HOC</h1></a>
+                          <a href="https://alensao.com/ad-hoc" style="text-transform : uppercase;" target="_blank"><h1>Comité AD-HOC</h1></a>
                             <br>
                             <br>
                             <p>En vue de la collaboration étroite entre l'Association des Lauréats de l'ENSAO (ALENSAO), et l'Association des Etudiants de l'ENSAO (ADE), et dans le but de faciliter la coopération et la synergie entre les deux organismes, un comité ad-hoc a vu le jour, comprenant des membres des deux organismes, ainsi que des membres externes. L'objectif principal de ce comité est d'œuvrer sur la multitude d'événements et de projets incluant les deux associations. 
