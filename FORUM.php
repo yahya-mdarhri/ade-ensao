d15e071527de5a179e439415bc05665b66afed93
yahya-mdarhri <yahyamdarhri00@gmail.com>
2021-05-04 23:11:30 +0000

    v15

--- FORUM.php
+++ FORUM.php
@@ -54,7 +54,7 @@
                                 <div class="col-md-3 col-sm-6 text-center animate-box">
                                     <span class="icon"><i class="icon-world"></i></span>
                                     <span class="fh5co-counter js-counter" data-from="0" data-to="3297" data-speed="5000" data-refresh-interval="50"></span>
-                                    <span class="fh5co-counter-label">ème édition</span>
+                                    <span class="fh5co-counter-label">10ème édition</span>
                                 </div>
                                 <div class="col-md-3 col-sm-6 text-center animate-box">
                                     <span class="icon"><i class="icon-study"></i></span>
@@ -189,30 +189,8 @@
 
 
 
+    <?php include("js.php"); ?>
 
-<!-- jQuery -->
-<script src="js/jquery.min.js"></script>
-<!-- jQuery Easing -->
-<script src="js/jquery.easing.1.3.js"></script>
-<!-- Bootstrap -->
-<script src="js/bootstrap.min.js"></script>
-<!-- Waypoints -->
-<script src="js/jquery.waypoints.min.js"></script>
-<!-- Stellar Parallax -->
-<script src="js/jquery.stellar.min.js"></script>
-<!-- Carousel -->
-<script src="js/owl.carousel.min.js"></script>
-<!-- Flexslider -->
-<script src="js/jquery.flexslider-min.js"></script>
-<!-- countTo -->
-<script src="js/jquery.countTo.js"></script>
-<!-- Magnific Popup -->
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script src="js/magnific-popup-options.js"></script>
-<!-- Count Down -->
-<script src="js/simplyCountdown.js"></script>
-<!-- Main -->
-<script src="js/main.js"></script>
 
 </body>
 </html>
