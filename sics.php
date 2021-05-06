0936fd103fe9cd76a0061def8d7c90c53df32ed4
yahya-mdarhri <yahyamdarhri00@gmail.com>
2021-05-06 04:06:15 +0000

    Update sics.php

--- sics.php
+++ sics.php
@@ -74,7 +74,7 @@ Nos missions :
             <div class="container">
                     <div class="row animate-box">
                         <div class="iframe-container">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/ktZZ2DH-28Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Ylv2Us5z9Lg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
                     </div>
             </div>  
