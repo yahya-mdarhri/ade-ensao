9fb27ff60cd27c1776427d2739f6d649dbfc458f
yahya-mdarhri <yahyamdarhri00@gmail.com>
2021-05-06 03:33:18 +0000

    Update club essie.php

--- club essie.php
+++ club essie.php
@@ -94,11 +94,12 @@ Ce club n’a cessé de faire ses biens autour de nous, quand on voit nos nouvea
             </div>
 
 
-            
+            <br>
+            <br>
             <div class="container">
                     <div class="row animate-box">
                         <div class="iframe-container">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/a7EKqob5VBU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/5zrGPY-M7vw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
                     </div>
             </div>     
